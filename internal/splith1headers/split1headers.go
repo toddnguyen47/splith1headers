cbbcd843f97f613798b73e9295217ee550e1a90f
Todd Nguyen <todd.nguyen47@gmail.com>
2022-06-11 20:17:09 -0700

    Add notes connection

--- internal/splith1headers/split1headers.go
+++ internal/splith1headers/split1headers.go
@@ -12,20 +12,28 @@ import (
 var constants = struct {
 	baseFileName string
 	outputFolder string
+	notes        string
+	href         string
 }{
-	baseFileName: "body",
+	baseFileName: "Body",
 	outputFolder: "output",
+	notes:        "Notes",
+	href:         "href",
 }
 
 type splitStruct struct {
-	elems [][]*etree.Element
-	index int
+	elems           [][]*etree.Element
+	index           int
+	hasNotesSection bool
+	reverseMap      map[string]int
 }
 
 func NewSplitStruct() splitStruct {
 	return splitStruct{
-		elems: make([][]*etree.Element, 0),
-		index: 0,
+		elems:           make([][]*etree.Element, 0),
+		index:           0,
+		hasNotesSection: false,
+		reverseMap:      make(map[string]int),
 	}
 }
 
@@ -35,41 +43,81 @@ func (s *splitStruct) Split(inputFile string) {
 	if err != nil {
 		fmt.Println("ERROR reading from file")
 	}
-	s.parseTree(&doc.Element)
-	// TODO: WRITE TO FILES
+
+	// Recursively parse elements
+	for _, childElem := range doc.Element.ChildElements() {
+		s.parseTree(childElem)
+	}
 	s.writeToFiles()
 }
 
 func (s *splitStruct) parseTree(root *etree.Element) {
-	if !isElemSkipped(root) {
-		elemToAppend := root
-		// If h1, advance to the next file
-		if root.Tag == "h1" {
-			s.elems = append(s.elems, make([]*etree.Element, 0))
-			s.index = len(s.elems) - 1
-		} else if root.Tag == "image" {
-			newElem := etree.NewElement("img")
-			newAttr := etree.Attr{
-				Key:   "src",
-				Value: root.SelectAttrValue("href", ""),
-			}
-			newElem.Attr = append(newElem.Attr, newAttr)
-			elemToAppend = newElem
-		}
 
-		s.elems[s.index] = append(s.elems[s.index], elemToAppend)
+	elemToAppend := root
+	// If h1, advance to the next file
+	if strings.EqualFold(elemToAppend.Tag, "h1") {
+		s.elems = append(s.elems, make([]*etree.Element, 0))
+		s.index = len(s.elems) - 1
+		// If notes
+		attributes := elemToAppend.Attr
+		for _, attribute := range attributes {
+			if strings.EqualFold(attribute.Key, "id") && strings.EqualFold(attribute.Value, constants.notes) {
+				s.hasNotesSection = true
+			}
+		}
+	} else if strings.EqualFold("div", elemToAppend.Tag) {
+		// Select image
+		imageElem := elemToAppend.FindElement("//svg/image")
+		newElem := etree.NewElement("img")
+		newAttr := etree.Attr{
+			Key:   "src",
+			Value: imageElem.SelectAttrValue(constants.href, ""),
+		}
+		newElem.Attr = append(newElem.Attr, newAttr)
+		elemToAppend = newElem
+	} else if strings.EqualFold("p", elemToAppend.Tag) {
+		for _, childElem := range elemToAppend.ChildElements() {
+			if strings.EqualFold(childElem.Tag, "sup") {
+				supId := childElem.SelectAttr("id")
+				if supId != nil {
+					s.reverseMap["#"+supId.Value] = s.index
+				}
+
+				aElem := childElem.FindElement("//sup/a")
+				attr := aElem.SelectAttr(constants.href)
+				if strings.Contains(attr.Value, "cite_note") {
+					attr.Value = fmt.Sprintf("Notes.xhtml%s", attr.Value)
+				}
+				aElem.CreateAttr(constants.href, attr.Value)
+			}
+		}
+	} else if strings.EqualFold("ol", elemToAppend.Tag) {
+		// For each <li> element
+		for _, childElem := range elemToAppend.ChildElements() {
+			if strings.EqualFold("li", childElem.Tag) {
+				aElem := childElem.FindElement("//li/span/a")
+				attr := aElem.SelectAttr(constants.href)
+				if index, ok := s.reverseMap[attr.Value]; ok {
+					attr.Value = fmt.Sprintf("%s%s", s.getFileName(index), attr.Value)
+					aElem.CreateAttr(constants.href, attr.Value)
+				}
+			}
+		}
 	}
+
+	s.elems[s.index] = append(s.elems[s.index], elemToAppend)
 
 	// Recursively parse elements
-	for _, childElem := range root.ChildElements() {
-		s.parseTree(childElem)
-	}
+	// for _, childElem := range root.ChildElements() {
+	// 	s.parseTree(childElem)
+	// }
 }
 
 func (s *splitStruct) writeToFiles() {
+	fmt.Printf("Map: %v\n", s.reverseMap)
 	os.Mkdir(constants.outputFolder, os.ModeDir)
 	for i, xmlInFile := range s.elems {
-		fileName := fmt.Sprintf("%s%02d.xhtml", constants.baseFileName, i)
+		fileName := s.getFileName(i)
 		fullFileName := path.Join(constants.outputFolder, fileName)
 		rootElem := etree.NewElement("div")
 		for _, xmlElem := range xmlInFile {
@@ -77,6 +125,7 @@ func (s *splitStruct) writeToFiles() {
 		}
 		newDoc := etree.NewDocument()
 		newDoc.SetRoot(rootElem)
+		newDoc.Indent(2)
 
 		fmt.Println("Writing to file " + fullFileName)
 		err := newDoc.WriteToFile(fullFileName)
@@ -86,26 +135,12 @@ func (s *splitStruct) writeToFiles() {
 	}
 }
 
-func isElemSkipped(root *etree.Element) bool {
-	// Empty tags
-	if len(root.Tag) <= 0 {
-		return true
-	}
+func (s *splitStruct) getFileName(index int) string {
+	fileName := fmt.Sprintf("%s%02d.xhtml", constants.baseFileName, index)
 
-	// Skip <svg> and <body>
-	if strings.EqualFold(root.Tag, "svg") || strings.EqualFold(root.Tag, "body") {
-		return true
+	// If last file and file has notes
+	if index == len(s.elems)-1 && s.hasNotesSection {
+		fileName = fmt.Sprintf("%s.xhtml", constants.notes)
 	}
-
-	// Skip <div class=svg_outer
-	if strings.EqualFold("div", root.Tag) {
-		attributes := root.Attr
-		for _, attribute := range attributes {
-			if strings.EqualFold(attribute.Key, "class") && strings.Contains(attribute.Value, "svg_outer") {
-				return true
-			}
-		}
-	}
-
-	return false
+	return fileName
 }
